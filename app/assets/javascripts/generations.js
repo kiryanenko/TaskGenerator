430396dd339cdc85214966943fc7ed5561110032
Alexander <kiryanenkoav@mail.ru>
2016-11-23 02:06:24 +0300

    get_html()

---
+++ app/assets/javascripts/generations.js
@@ -0,0 +1,15 @@
+function get_html(el_id, link) {
+    var el = document.getElementById(el_id);
+    var status = el.getElementsByClassName('view-status')[0];
+    status.innerHTML = '<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>';
+    $.ajax({
+        type: 'get',                                    //тип запроса: get,post либо head
+        url: link,                                      //url адрес файла обработчика
+        data: {},                                       //параметры запроса
+        response: 'text',                               //тип возвращаемого ответа text либо xml
+        success: function (data) {                      //возвращаемый результат от сервера
+            status.innerHTML = '';
+            el.getElementsByClassName('view-result')[0].innerHTML = data;
+        }
+    });
+}
