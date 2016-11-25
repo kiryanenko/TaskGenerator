f927964e0e488b3f24be879a20f23a9eee3a5ed0
Alexander <kiryanenkoav@mail.ru>
2016-11-26 02:31:32 +0300

    add errors

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -1,4 +1,10 @@
 class Variable < ApplicationRecord
   belongs_to :task
-  validates :name, format: { with: /^\w+$/, message: 'Не верное имя переменной' }
+  validates :name, format: { with: /\A\w+\Z/, message: 'Не верное имя переменной' }
+  validates_each :from, :to do |record, attr, value|
+    record.errors.add(attr, 'Верхняя граница должна быть больше чем нижния') unless record.from <= record.to
+  end
+  validates_each :round do |record, attr, value|
+    record.errors.add(attr, 'Округление должно быть больше или равно 0') unless value.nil? or value >= 0
+  end
 end
