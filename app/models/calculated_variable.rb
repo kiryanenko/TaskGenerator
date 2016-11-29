e75cdd3ad036e458b196d1bfaf781a3503fac183
Alexander <kiryanenkoav@mail.ru>
2016-11-30 00:09:24 +0300

    fixes

--- app/models/calculated_variable.rb
+++ app/models/calculated_variable.rb
@@ -1,4 +1,4 @@
 class CalculatedVariable < ApplicationRecord
   belongs_to :task
-  validates :name, format: { with: /\A\w+\Z/, message: 'Не верное имя переменной' }
+  validates :name, format: { with: /\A\w+\Z/, message: 'Неверное имя переменной' }
 end
